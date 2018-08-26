82da52fe579a9a89ebaf9143308e1f10f3f00f65
okubo <mag.cupw@gmail.com>
2018-08-27 00:20:38 +0900

    first commit

---
+++ src/main/java/SecurityConfig.java
@@ -0,0 +1,7 @@
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
